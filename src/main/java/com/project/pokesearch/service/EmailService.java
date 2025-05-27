339ba330488d7229cf82f36ac34f6c3a3786430a
Dorian <dorianzasada@gmail.com>
2025-05-27 20:32:37 +0200

    Fixed a critical issue with annotation parsing

--- src/main/java/com/project/pokesearch/service/EmailService.java
+++ src/main/java/com/project/pokesearch/service/EmailService.java
@@ -1,26 +1,26 @@
 package com.project.pokesearch.service;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Value;
+// Upewnij się, że importy są poprawne
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 @Slf4j
 public class EmailService {
-    
-    private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.from}")
-    private String mailFrom;
-    
+    private final JavaMailSender mailSender;
+    private final String mailFrom;
+
+    public EmailService(JavaMailSender mailSender, @Value("${spring.mail.from}") String mailFrom) {
+        this.mailSender = mailSender;
+        this.mailFrom = mailFrom;
+    }
+
     public void sendEmail(String to, String subject, String content) {
-        
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(mailFrom);
@@ -30,9 +30,7 @@ public class EmailService {
             mailSender.send(message);
             log.info("Email sent to {}", to);
         } catch (MailException e) {
-            log.error("Niew udało się wysłać e-maila na adres: {}. Błąd: {}", to, e.getMessage());
+            log.error("Nie udalo sie wyslac e-maila na adres: {}. Blad: {}", to, e.getMessage());
         }
     }
-    
-    
 }
